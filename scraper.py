fc95592017b7d287426b2a9a91e4908bbd8cdf60
Nader Jemaa <n.jemaa@instadeep.com>
2021-12-20 10:21:13 +0100

    fixing pylint

--- scraper.py
+++ scraper.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
-import mechanicalsoup
-import pandas as pd
-import sqlite3
-
 """Scraper.py: this is a web scrapper."""
 
-__author__      = "Nader Jemaa"
-__copyright__   = "Copyright 2021, Tunisia"
+import mechanicalsoup
+#import pandas as pd
+#import sqlite3
 
-baseUrl = "https://nationaltoday.com"
-months = ["january","february","march","april","may","june","july","august","september","october","november","december"]
+__author__ = "Nader Jemaa"
+__copyright__ = "Copyright 2021, Tunisia"
 
-browser = mechanicalsoup.StatefulBrowser()
+BASE_URL = "https://nationaltoday.com"
+MONTHS = ["january", "february", "march", "april", "may", "june", "july", "august", "september",
+          "october", "november", "december"]
 
-def get_holidays_by_month(month, day):
+BROWSER = mechanicalsoup.StatefulBrowser()
+
+def get_holidays_by_month(month_str, day_int):
+    """get all holidays by month and day"""
     final_url = ""
-    if month.lower() in months:
-        final_url = baseUrl + "/" + month.lower() + "-" + str(day) + "-holidays"
-    browser.open(final_url)
-    holiday_title = browser.page.find_all("h3", attrs = {"class":"holiday-title"})
+    if month_str.lower() in MONTHS:
+        final_url = BASE_URL + "/" + month_str.lower() + "-" + str(day_int) + "-holidays"
+    BROWSER.open(final_url)
+    holiday_title = BROWSER.page.find_all("h3", attrs={"class":"holiday-title"})
     holiday_title_array = [value.text for value in holiday_title]
-    holiday_desc = browser.page.find_all("p", attrs = {"class":"excerpt"})
+    holiday_desc = BROWSER.page.find_all("p", attrs={"class":"excerpt"})
     holiday_desc_array = [value.text for value in holiday_desc]
 
-    for i in range(len(holiday_title_array)):
-        print(holiday_title_array[i] + ": " + holiday_desc_array[i])
+    for i, title in enumerate(holiday_title_array):
+        print(title + ": " + holiday_desc_array[i])
     print(" ")
-    
 
-for month in months:
+for month in MONTHS:
     for day in range(31):
         print(month + "," + str(day) + ": ")
         get_holidays_by_month("january", day)
-
-
